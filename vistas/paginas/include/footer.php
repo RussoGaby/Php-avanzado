ee4a9a820d3a3c928dce67cbb884f5da18d9041e
Gabriel Carrión Pintos <gcarrionpintos@gmail.com>
2023-08-03 16:05:44 -0300

    Php avanzado

---
+++ vistas/paginas/include/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Gabriel Nicolás Carrión Pintos</p>
+    <p>PHP y MySQL avanzado UTN</p>
+    <p>Promoción: 999191538</p>
+</footer>
---
+++ vistas/paginas/include/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="over"></div>
+    <div class="titulo">
+        <h1>PHP y MySQL Avanzado</h1>
+        <h2></h2>
+    </div>
+</header>
